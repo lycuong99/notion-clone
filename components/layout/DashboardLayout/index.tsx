52e0023adb657769fcbe3dba043589dc909b61fa
lycuong99 <lycuong99@gmail.com>
2024-04-26 15:57:43 +0700

    update

--- components/layout/DashboardLayout/index.tsx
+++ components/layout/DashboardLayout/index.tsx
@@ -8,9 +8,11 @@ import { ImperativePanelHandle } from "react-resizable-panels";
 
 import { RxDoubleArrowLeft, RxDoubleArrowRight } from "react-icons/rx";
 import { Createpage, Menu } from "@/components/icon";
+import { cn } from "@/lib/utils";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const [open, setOpen] = useState(true);
+  const [isExpanded, setExpanded] = useState(true);
+  const [open, setOpen] = useState(false);
   const [menuHover, setMenuHover] = useState(false);
   const [collapsible, setCollapsible] = useState(true);
   const handleDragging = useCallback(() => {
@@ -18,12 +20,12 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   }, [collapsible]);
 
   useLayoutEffect(() => {
-    if (open) {
+    if (isExpanded) {
       sidebarPanelRef.current?.expand();
     } else {
       sidebarPanelRef.current?.collapse();
     }
-  }, [open]);
+  }, [isExpanded]);
   const sidebarPanelRef = useRef<ImperativePanelHandle>(null);
 
   const { user } = useUser();
@@ -38,8 +40,13 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
         maxSize={40}
         minSize={16}
       >
-        <div className="flex flex-col p-2">
-          <div className="flex justify-between hover:bg-accent hover:text-accent-foreground px-2 py-2 gap-2 rounded-md">
+        <div
+          className={cn("flex flex-col p-2", {
+            "fixed h-[90vh] rounded-lg w-[20vw] translate-y-[5vh] transition-transform duration-300 shadow-xl rounded-r-md":
+              !isExpanded,
+          })}
+        >
+          <div className="flex justify-between hover:bg-accent hover:text-accent-foreground px-2 py-2 gap-2 ">
             <div className="flex items-center gap-1">
               <Avatar className="w-6 h-6">
                 <AvatarImage src={user?.imageUrl} />
@@ -47,19 +54,19 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
               <p>{user?.fullName}</p>
             </div>
             <div className="flex gap-1 ">
-              <Button
-                variant={"outline"}
-                className="bg-transparent hover:bg-slate-300"
-                size={"icon-sm"}
-                onClick={() => setOpen(!open)}
-              >
-                <RxDoubleArrowLeft />
-              </Button>
-              {open && (
-                <Button variant={"outline"} className="bg-transparent hover:bg-slate-300" size={"icon-sm"}>
-                  <Createpage className="font-bold" />
+              {isExpanded && (
+                <Button
+                  variant={"outline"}
+                  className="bg-transparent hover:bg-slate-300"
+                  size={"icon-sm"}
+                  onClick={() => setExpanded(!isExpanded)}
+                >
+                  <RxDoubleArrowLeft />
                 </Button>
               )}
+              <Button variant={"outline"} className="bg-transparent hover:bg-slate-300" size={"icon-sm"}>
+                <Createpage className="font-bold" />
+              </Button>
             </div>
           </div>
         </div>
@@ -67,16 +74,16 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <ResizableHandle onDragging={handleDragging} />
       <ResizablePanel className="overflow-auto h-screen">
         <nav className="flex p-2">
-          {!open && (
+          {!isExpanded && (
             <Button
               variant={"outline"}
               className="bg-transparent hover:bg-slate-300"
               size={"icon-sm"}
-              onClick={() => setOpen(!open)}
+              onClick={() => setExpanded(!isExpanded)}
               onMouseEnter={() => setMenuHover(true)}
               onMouseLeave={() => setMenuHover(false)}
             >
-              {menuHover ? <RxDoubleArrowRight /> : <Menu />}
+              {isExpanded ? <RxDoubleArrowRight /> : <Menu />}
             </Button>
           )}
         </nav>
