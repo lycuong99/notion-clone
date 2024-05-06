cb50c36b07d6f4a03a1a49c812bff8cd91b98021
lycuong99 <lycuong99@gmail.com>
2024-05-06 09:23:29 +0700

    update

--- components/layout/DashboardLayout/index.tsx
+++ components/layout/DashboardLayout/index.tsx
@@ -1,11 +1,5 @@
 "use client";
-import {
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
 
 import { cn } from "@/lib/utils";
 import gsap from "gsap";
@@ -14,51 +8,12 @@ import { Draggable } from "gsap/Draggable";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
 import { Sidebar } from "./Sidebar";
 import { Header } from "./Header";
-
+import { useGSAP } from "@gsap/react";
 gsap.registerPlugin(Flip, Draggable);
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const [isExpanded, setExpanded] = useState(true);
   const [open, setOpen] = useState(true);
-
-  useLayoutEffect(() => {
-    if (sidebarRef.current && resizeHanleRef.current) {
-      gsap.set(resizeHanleRef.current, {
-        x: sidebarRef.current.clientWidth,
-      });
-    }
-    Draggable.create("[data-gsap-resize-handle]", {
-      type: "x",
-      cursor: "col-resize",
-      activeCursor: "col-resize",
-      bounds: { minX: 200, maxX: 500 },
-      onDrag: () => {
-        console.log("newWidth");
-        if (sidebarRef.current && resizeHanleRef.current) {
-          let newWidth =
-            resizeHanleRef.current?.getBoundingClientRect().left +
-            resizeHanleRef.current?.offsetWidth;
-          console.log("newWidth xx", newWidth);
-
-          sidebarRef.current.style.width = `${newWidth}px`;
-        }
-      },
-      onDragStart: () => {
-        startResizeRef.current = true;
-      },
-      onDragEnd: () => {
-        startResizeRef.current = false;
-      },
-
-      liveSnap: {
-        x: function (value) {
-          //snap to the closest increment of 50.
-          return Math.round(value / 10) * 10;
-        },
-      },
-      inertia: true,
-    });
-  }, []);
 
   const resizeHanleRef = useRef<HTMLDivElement>(null);
   const startResizeRef = useRef<boolean>(false);
@@ -66,15 +21,11 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const contentRef = useRef<HTMLDivElement>(null);
 
   const { isMobile } = useMediaQuery();
+  const handleExpand = () => {
+    setExpanded(!isExpanded);
+  };
 
-  useLayoutEffect(() => {
-    if (isMobile) {
-        gsap.set(sidebarRef.current, {
-            position:'absolute',
-        });
-        
-    };
-
+  useGSAP(() => {
     if (sidebarRef.current && resizeHanleRef.current) {
       gsap.set(resizeHanleRef.current, {
         x: sidebarRef.current.clientWidth,
@@ -86,12 +37,8 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       activeCursor: "col-resize",
       bounds: { minX: 200, maxX: 500 },
       onDrag: () => {
-        console.log("newWidth");
         if (sidebarRef.current && resizeHanleRef.current) {
-          let newWidth =
-            resizeHanleRef.current?.getBoundingClientRect().left +
-            resizeHanleRef.current?.offsetWidth;
-          console.log("newWidth xx", newWidth);
+          let newWidth = resizeHanleRef.current?.getBoundingClientRect().left + resizeHanleRef.current?.offsetWidth;
 
           sidebarRef.current.style.width = `${newWidth}px`;
         }
@@ -111,67 +58,69 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       },
       inertia: true,
     });
-
-    return ()=>{
-        gsap.set(sidebarRef.current, {
-            clearProps:'all',
-        });
-        gsap.set(contentRef.current, {
-            clearProps:'all',
-        })
-        setOpen(true)
-        setExpanded(true)
-    }
-  }, [isMobile]);
-
-  const handleExpand = () => {
-    setExpanded(!isExpanded);
-  };
+  });
 
   useLayoutEffect(() => {
+    // setExpanded(true);
+    console.log("SET EXPANDED TO TRUE");
+
+    if (isMobile) {
+      gsap.set(sidebarRef.current, {
+        position: "absolute",
+        top: 0,
+      });
+      gsap.set(sidebarRef.current, {
+        x: 0,
+        opacity: 1,
+      });
+    } else {
+      gsap.set(sidebarRef.current, {
+        clearProps: "position,top,opacity,x",
+      });
+      gsap.set(contentRef.current, {
+        clearProps: "transform",
+      });
+      // setExpanded(false);
+      setOpen(true);
+    }
+
+    // return () => {
+    //   gsap.set(sidebarRef.current, {
+    //     clearProps: "all",
+    //   });
+    //   gsap.set(contentRef.current, {
+    //     clearProps: "all",
+    //   });
+    //   setOpen(true);
+    //   setExpanded(true);
+    // };
+  }, [isMobile]);
+
+  useGSAP(() => {
     if (sidebarRef.current) {
       let state = Flip.getState([sidebarRef.current, contentRef.current]);
-      // let contentState = Flip.getState(contentRef.current);
+      const classList = ["md:fixed", "z-10", "top-[80px]", "bg-white", "rounded-r-lg", "shadow-md"];
 
-      const handleExpand = () => {
-        const classList = [
-          "md:fixed",
-          "z-10",
-          "top-[80px]",
-          "bg-white",
-          "rounded-r-lg",
-          "shadow-md",
-        ];
+      const clearExpand = () => {
+        if (!sidebarRef.current) return;
+        sidebarRef.current.classList.remove(...classList);
+        sidebarRef.current.style.height = `100vh`;
+      };
+      const expand = () => {
         if (!sidebarRef.current) return;
         if (!isExpanded) {
           sidebarRef.current.classList.add(...classList);
           sidebarRef.current.style.height = `calc(100vh - 160px)`;
         } else {
-          sidebarRef.current.classList.remove(...classList);
-          sidebarRef.current.style.height = `100vh`;
-        }
-      };
-      const handleExpandOnMobile = () => {
-        if (!sidebarRef.current) return;
-        // const classList = [
-        //     "-translate-x-full",
-        // ]
-        if (!isExpanded) {
-          // sidebarRef.current.classList.add(...classList);
-          gsap.set(sidebarRef.current, {
-            translateX: "-100%",
-          });
-        } else {
-          // gsap.set(sidebarRef.current, {
-          //     translateX: "-100%",
-          //   })
+          clearExpand();
         }
       };
 
+      console.log("Expanded - ", isExpanded);
       if (isMobile) {
-        handleExpandOnMobile();
+        clearExpand();
       } else {
-        handleExpand();
+        expand();
       }
 
       Flip.from(state, {
@@ -183,6 +132,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       });
     }
   }, [isExpanded, isMobile]);
+
   const handleMousemove = useCallback(
     (e: MouseEvent) => {
       let condition = !isExpanded && !open;
@@ -205,45 +155,51 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   );
 
   useLayoutEffect(() => {
-    if (isMobile) return;
+    // if (isMobile) return;
     document.addEventListener("mousemove", handleMousemove);
     return () => {
       document.removeEventListener("mousemove", handleMousemove);
     };
-  }, [handleMousemove, isMobile]);
+  }, [handleMousemove]);
 
-  useLayoutEffect(() => {
-    if (sidebarRef.current) {
-      if (isMobile) {
-        gsap.to(contentRef.current, {
-          x: open ? sidebarRef.current.clientWidth : 0,
-          delay: 0.2,
-          duration: 0.3,
-          ease: "power1.out",
-        });
-      } else {
-        gsap.to(sidebarRef.current, {
-          x: open ? 0 : "-100%",
-          delay: 0.2,
-          duration: 0.3,
-          opacity: open ? 1 : 0,
-          ease: "power1.out",
-        });
+  useGSAP(
+    () => {
+      if (sidebarRef.current) {
+        console.log("Open - ", open);
+
+        if (isMobile) {
+          gsap.to(contentRef.current, {
+            x: open ? sidebarRef.current.clientWidth : 0,
+            delay: 0.2,
+            duration: 0.3,
+            ease: "power1.out",
+          });
+        } else {
+          gsap.set(contentRef.current, {
+            x: 0,
+          });
+          gsap.to(sidebarRef.current, {
+            x: open ? 0 : "-100%",
+            delay: 0.2,
+            duration: 0.3,
+            opacity: open ? 1 : 0,
+            ease: "power1.out",
+          });
+        }
       }
+    },
+    {
+      dependencies: [open, isMobile],
+      // revertOnUpdate: true,
     }
-  }, [open, isMobile]);
+  );
 
   const handleOpen = () => {
     setOpen(!open);
   };
   return (
     <div className="flex h-screen overflow-hidden relative">
-      <div
-        className={cn(
-          "w-[300px] bg-slate-100 h-full z-10 md:z-20 origin-top border"
-        )}
-        ref={sidebarRef}
-      >
+      <div className={cn("w-[300px] bg-slate-100 h-full z-10 md:z-20 origin-top border")} ref={sidebarRef}>
         <Sidebar isExpanded={isExpanded} handleExpand={handleExpand} />
 
         {!isMobile && (
@@ -256,9 +212,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       </div>
 
       <div
-        className={cn(
-          "w-full md:flex-1 flex flex-col h-full origin-center-right z-20 md:z-10 bg-white"
-        )}
+        className={cn("w-full md:flex-1 flex flex-col h-full origin-center-right z-20 md:z-10 bg-white")}
         ref={contentRef}
       >
         <Header
